192d8c86889329597759d8e36cae275153c09c06
JCRN <clemons.jason@gmail.com>
2019-09-04 11:34:43 -0700

    built abstractanimal constructor

--- src/animalkingdom/AbstractAnimal.java
+++ src/animalkingdom/AbstractAnimal.java
@@ -1,16 +1,21 @@
 package animalkingdom;
 
 public abstract class AbstractAnimal {
-    // Fields
-    int food;
 
-    // Constructors
-    public AbstractAnimal(int food) {
-        this.food = food;
-    }
+    // Fields:
+    int id;
+    int maxId = 0;
+    int yearDiscovered;
+    String name;
 
-    public AbstractAnimal() {
-        food = 1;
+    // Constructor:
+    public AbstractAnimal(String name, int yearDiscovered) {
+        // Provide uinque Id for each instance
+        maxId++;
+        id = maxId;
+
+        this.name = name;
+        this.yearDiscovered = yearDiscovered;
     }
 
 }
