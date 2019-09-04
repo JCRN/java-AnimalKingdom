9a0e7f83fb955b9b7f1c21ea8efc313f462ea04a
JCRN <clemons.jason@gmail.com>
2019-09-04 12:53:19 -0700

    created / populated arraylist<abstractanimal>

--- src/animalkingdom/Main.java
+++ src/animalkingdom/Main.java
@@ -3,6 +3,7 @@ package animalkingdom;
 public class Main {
     public static void main(String[] args) {
         // Instantiate classes
+
         // Mammals:
         Mammal panda = new Mammal("Panda", 1869);
         Mammal zebra = new Mammal("Zebra", 1778);
@@ -23,6 +24,24 @@ public class Main {
         Fish salmon = new Fish("Salmon", 1758);
         Fish catfish = new Fish("Catfish", 1817);
         Fish perch = new Fish("Perch", 1758);
+
+        // Create / populate ArrayList<AbstractAnimal>
+        ArrayList<AbstractAnimal> animalsList = new ArrayList<AbstractAnimal>();
+        animalsList.add(panda);
+        animalsList.add(zebra);
+        animalsList.add(koala);
+        animalsList.add(sloth);
+        animalsList.add(armadillo);
+        animalsList.add(raccoon);
+        animalsList.add(bigfoot);
+        animalsList.add(pigeon);
+        animalsList.add(peacock);
+        animalsList.add(toucan);
+        animalsList.add(parrot);
+        animalsList.add(swan);
+        animalsList.add(salmon);
+        animalsList.add(catfish);
+        animalsList.add(perch);
 
     }
 }
